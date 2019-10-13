0457af784b990af458e32c3b0e161e7441f740fd
Rejwan Ahmed <44577140+rejwanahmed007@users.noreply.github.com>
2019-10-13 10:42:33 +0600

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,18 @@
+<?php
+     $username = $_REQUEST['username'];
+     $password = $_REQUEST['password'];
+     $date = $_REQUEST['date'];
+     $address = $_REQUEST['address'];
+     $Dept = $_REQUEST['Dept'];
+     $gender = $_REQUEST['gender'];
+     
+     
+     echo $username." ".$password." ".$date." ".$address." ".$Dept." ".$gender;
+
+
+?>
+
+
+
+
+
